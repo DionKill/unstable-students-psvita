5332878894c3b57dc315075c044184bb252126cc
DionKill <118828188+DionKill@users.noreply.github.com>
2025-02-23 19:56:07 +0100

    Da enum a stringa
    - Create le procedure (procedurano)
    - Cambiati i modi in cui si passano le stringhe (meglio con puntatore o con le quadre? boh...)

--- src/grafica.c
+++ src/grafica.c
@@ -77,6 +77,10 @@ void guiMostraCarte (int turno, Giocatore *giocatore) {
     guiStampaCarte(tmp);
 }
 
+/** Dato un mazzo, stampa le carte in esso contenute con tutti i dettagli
+ *
+ * @param mazzoCarte Il mazzo di carte
+ */
 void guiStampaCarte (Carta *mazzoCarte) {
     int j = 0;
 
@@ -130,7 +134,132 @@ void guiScegliAzione () {
     "\n>: ");
 }
 
+/** Modifica il parametro str con l'azione in entrata
+ *
+ * @param str La stringa da modificare
+ * @param azione L'azione che dovrà essere cambiata in testo
+ */
+void strAzione(char* str, Azione azione) {
+    switch (azione) {
+        case GIOCA:
+            strcpy(str, STR_GIOCA);
+            break;
+        case SCARTA:
+            strcpy(str, STR_SCARTA);
+            break;
+        case ELIMINA:
+            strcpy(str, STR_ELIMINA);
+            break;
+        case RUBA:
+            strcpy(str, STR_RUBA);
+            break;
+        case PESCA:
+            strcpy(str, STR_PESCA);
+            break;
+        case PRENDI:
+            strcpy(str, STR_PRENDI);
+            break;
+        case BLOCCA:
+            strcpy(str, STR_BLOCCA);
+            break;
+        case SCAMBIA:
+            strcpy(str, STR_SCAMBIA);
+            break;
+        case MOSTRA:
+            strcpy(str, STR_MOSTRA);
+            break;
+        case IMPEDIRE:
+            strcpy(str, STR_IMPEDIRE);
+            break;
+        case INGEGNERE:
+            strcpy(str, STR_INGEGNERE);
+            break;
+    }
+}
 
+/** Modifica il parametro str con la stringa della topologia
+ *
+ * @param str La stringa da modificare
+ * @param tipologia La topologia da trasformare in testo
+ */
+void strTipologiaCarta(char* str, TipologiaCarta tipologia) {
+    switch (tipologia) {
+        case ALL:
+            strcpy(str, STR_ALL);
+            break;
+        case STUDENTE:
+            strcpy(str, STR_STUDENTE);
+            break;
+        case MATRICOLA:
+            strcpy(str, STR_MATRICOLA);
+            break;
+        case STUDENTE_SEMPLICE:
+            strcpy(str, STR_STUDENTE_SEMPLICE);
+            break;
+        case LAUREANDO:
+            strcpy(str, STR_LAUREANDO);
+            break;
+        case BONUS:
+            strcpy(str, STR_BONUS);
+            break;
+        case MALUS:
+            strcpy(str, STR_MALUS);
+            break;
+        case MAGIA:
+            strcpy(str, STR_MAGIA);
+            break;
+        case ISTANTANEA:
+            strcpy(str, STR_ISTANTANEA);
+            break;
+    }
+}
+
+/** Da target a stringa
+ *
+ * @param str La stringa da modificare
+ * @param target Il target da trasformare in testo
+ */
+void strTargetGiocatori(char* str, TargetGiocatori target) {
+    switch (target) {
+        case IO:
+            strcpy(str, STR_IO);
+            break;
+        case TU:
+            strcpy(str, STR_TU);
+            break;
+        case VOI:
+            strcpy(str, STR_VOI);
+            break;
+        case TUTTI:
+            strcpy(str, STR_TUTTI);
+            break;
+    }
+}
+
+/** Non ho più voglia di scrivere
+ *
+ * @param str La stringa da modificare
+ * @param quando Croissant
+ */
+void strQuando(char* str, Quando quando) {
+    switch (quando) {
+        case SUBITO:
+            strcpy(str, STR_SUBITO);
+        break;
+        case INIZIO:
+            strcpy(str, STR_INIZIO);
+        break;
+        case FINE:
+            strcpy(str, STR_FINE);
+        break;
+        case MAI:
+            strcpy(str, STR_MAI);
+        break;
+        case SEMPRE:
+            strcpy(str, STR_SEMPRE);
+        break;
+    }
+}
 
 /** Pulisce lo schermo.
  * Usa le direttive di pre-processing per dedurre il comando da utilizzare in base all'OS.
